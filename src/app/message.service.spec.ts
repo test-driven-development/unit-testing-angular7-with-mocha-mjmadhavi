4ea23ccb03e14ef5642ffc5a33ba0b0a5dbbc72e
AA Developer <developer@aa.com>
2019-01-03 10:54:39 -0600

    message service AAA and first test green

--- src/app/message.service.spec.ts
+++ src/app/message.service.spec.ts
@@ -1,9 +1,12 @@
 import {MessageService} from './message.service';
 
 describe('MessageService', () => {
+  let service;
+  beforeEach(() => {
+    service = new MessageService();
+  });
   it('should have no messages to start', () => {
-    const service = new MessageService();
-    (service.messages.length).should.equal(1);
+    (service.messages.length).should.equal(0);
   });
   it('should add a message when add is called');
   it('should clear all messages when clear is called');
