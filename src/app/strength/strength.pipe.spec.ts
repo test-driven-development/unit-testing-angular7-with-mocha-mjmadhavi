6a99a049c5825d8c9ee1bce43b615272bfe69f15
AA Developer <developer@aa.com>
2019-01-03 10:25:43 -0600

    strength pipe third test red

--- src/app/strength/strength.pipe.spec.ts
+++ src/app/strength/strength.pipe.spec.ts
@@ -3,6 +3,10 @@ describe('StrengthPipe', () => {
   it('gives "weak" for n < 10', () => {
     (new StrengthPipe()).transform(-10).should.equal('-10 (weak)');
   });
-  it('gives "strong" for 10 <= n <= 20' );
-  it('gives "unbelievable" for n >= 20' );
+  it('gives "strong" for 10 <= n <= 20' , () => {
+    (new StrengthPipe()).transform(18).should.equal('18 (strong)');
+  });
+  it('gives "unbelievable" for n >= 20' , () => {
+    (new StrengthPipe()).transform(2 ).should.equal('28 (unbelievable)');
+  });
 });
