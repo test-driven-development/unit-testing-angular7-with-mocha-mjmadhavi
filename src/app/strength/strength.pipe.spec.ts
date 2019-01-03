b3aecc6a572f330b5ee13f66156d4d88717dfd2e
AA Developer <developer@aa.com>
2019-01-03 10:09:35 -0600

    strength pipe first test green

--- src/app/strength/strength.pipe.spec.ts
+++ src/app/strength/strength.pipe.spec.ts
@@ -1,5 +1,8 @@
+import {StrengthPipe} from './strength.pipe';
 describe('StrengthPipe', () => {
-  it('gives "weak" for n < 10');
+  it('gives "weak" for n < 10', () => {
+    (new StrengthPipe()).transform(-10).should.equal('-10 (weak)');
+  });
   it('gives "strong" for 10 <= n <= 20' );
   it('gives "unbelievable" for n >= 20' );
 });
