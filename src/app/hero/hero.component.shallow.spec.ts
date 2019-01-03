b1fd074a6da3e7ed385dcfb9585743181ec45538
AA Developer <developer@aa.com>
2019-01-03 17:35:41 -0600

    HeroComponent handle errors:template parse, test module already instantiated

--- src/app/hero/hero.component.shallow.spec.ts
+++ src/app/hero/hero.component.shallow.spec.ts
@@ -1,4 +1,4 @@
-import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {ComponentFixture, getTestBed, TestBed} from '@angular/core/testing';
 import {HeroComponent} from './hero.component';
 import {NO_ERRORS_SCHEMA} from '@angular/core';
 
@@ -12,11 +12,17 @@ describe('Hero Component (Shallow Tests)', () => {
       declarations: [HeroComponent],
       // work around for Template parse errors:
       // Can't bind to 'routerLink' since it isn't a known property of 'a'
+      // ignore unknown attribute or element in template html
       schemas: [NO_ERRORS_SCHEMA]
     });
 
     // create component fixture - component wrapper
     fixture = TestBed.createComponent(HeroComponent);
+  });
+
+  // handle Error:cannot configure the test module when the test module has already been instantiated.
+  afterEach(() => {
+    getTestBed().resetTestingModule();
   });
 
   it('has a passing canary test',  () => {
