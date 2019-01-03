427c39626c13308c5c8a4d826ad719f487255716
AA Developer <developer@aa.com>
2019-01-03 10:43:13 -0600

    message service design clear test added

--- src/app/message.service.spec.ts
+++ src/app/message.service.spec.ts
@@ -1,4 +1,5 @@
 describe('MessageService', () => {
   it('should have no messages to start');
   it('should add a message when add is called');
+  it('should clear all messages when clear is called');
 });
