01b266740f2da4f7c4efc38db1b68f18f36f3c6c
AA Developer <developer@aa.com>
2019-01-03 11:32:00 -0600

    Heroes component spec design

---
+++ src/app/heroes/heroes.component.spec.ts
@@ -0,0 +1,8 @@
+describe('Heroes Component', () => {
+  it('has passing canary test', () => {
+    true.should.be.true('automated test infrastructure not working');
+  });
+  it('collaborates with the heroes service to get heroes');
+  it('collaborates with the heroes service to delete hero');
+  it('collaborates with the heroes service to add hero');
+});
